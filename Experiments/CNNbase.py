ada3ae8ae0036d555bc0fd90b02d191a88bd494a
janusz <jawi@itu.dk>
2024-04-26 02:22:20 +0200

    Some progress and removed coubled data in experiments

--- Experiments/CNNbase.py
+++ Experiments/CNNbase.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from numpy import float32
-import torch
 import torch.nn as nn
-import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+from torch.optim import Adam
 from torch.nn.functional import relu
 
 # Basic CNN 
-class CNNClassifier(nn.Module):
+class Mnist_CNN_Classifier(nn.Module):
     def __init__(self):
-        super(CNNClassifier, self).__init__()
+        super(Mnist_CNN_Classifier, self).__init__()
         self.linear1 = nn.Linear(784,250)
         self.linear2 = nn.Linear(250,100)
         self.linear3 = nn.Linear(100,10)
@@ -48,3 +47,45 @@ def cifarToDf (path:str = "../data/cifar-10-python\data_batch_1"):
     cifar_df['label'] = cifar[b'labels']
 
     return cifar_df
+
+def train(path:str, local_data:bool = False):
+    if local_data: # use locally stored mnist data 
+        data = pd.read_csv("../data/Mnist/mnist_train.csv")
+        dataset = ImageDFDataset(data)
+        dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    else: # Download and use Pytorch's Mnist data
+        from torchvision.datasets import mnist
+        dataloader = mnist(root='./data', train=True,
+                    download=True)
+
+    # Create an instance of your CNN model
+    model = Mnist_CNN_Classifier()
+
+    # Define the loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=0.001)
+
+    num_epochs = 10
+
+    # Training loop
+    for epoch in range(num_epochs):
+        for batch in dataloader:
+            inputs, labels = batch
+
+            # Forward pass
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Print the loss for monitoring the training progress
+            print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+def main():
+
+
+if __name__ == '__main__':
+    main()
+    print("CNN has been trained")
