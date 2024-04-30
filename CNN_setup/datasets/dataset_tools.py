722805bdbf3fece5b5ff2afe19ca8cde7e5d5cc9
janusz <jawi@itu.dk>
2024-04-30 07:02:32 +0200

    created new rotated and splitted CIFAR datasets

--- CNN_setup/datasets/dataset_tools.py
+++ CNN_setup/datasets/dataset_tools.py
@@ -1,4 +1,5 @@
 from torchvision.datasets import ImageFolder
+from PIL import Image
 import os
 
 def split_data_by_class(data, class_index):
@@ -11,3 +12,11 @@ def split_data_by_class(data, class_index):
             wo_class.append((i,l))
 
     return w_class, wo_class
+
+def save_dataset(data, path:str = 'data/transformed/cifar-div-by-class', transform = None, args = ()):
+    for i, (img, label) in enumerate(data):
+        # Apply the transformation if one was provided
+        if transform is not None:
+            img = transform(img,  *args)
+        os.makedirs(f'{path}/{label}', exist_ok=True)
+        img.save(f'{path}/{label}/img_{i}.png')
